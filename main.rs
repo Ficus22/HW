6a66882ac746eacc08607b0e27f714cc15758963
Ficus22 <130400795+Ficus22@users.noreply.github.com>
2024-10-16 18:21:14 +0200

    Update fonction exemple
    Correction de la fonction exemple et ajout de l'utilisation de pin_mode_input() à la fonction exemple.

--- main.rs
+++ main.rs
@@ -51,8 +51,8 @@ fn digital_write(pin: u8, value: bool) {
 
 
 /* FONCTION EXEMPLE
-Pour l'exemple, nous allons configurer la broche 2 comme sortie, la mettre à un état haut et
-lire son état
+Pour l'exemple, nous allons configurer la broche 2 comme sortie et la mettre à un état haut
+Nous allons également configurer la broche 3 comme entrée et lire son état
 */
 
 #[no_mangle]
@@ -60,5 +60,7 @@ pub extern "C" fn main() -> ! {
     
     pin_mode_output(2); // Configure la broche 2 comme sortie
     digital_write(2, true); // Met la broche 2 à l'état haut
-    let state = digital_read(2); // Lit l'état de la broche 2 et le stock dans state
+
+    pin_mode_input(3); // Configure la broche 3 comme entrée
+    let state = digital_read(3); // Lit l'état de la broche 3
 }
